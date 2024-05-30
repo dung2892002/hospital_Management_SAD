7cdf4a1cb829c228cf421911434e333a4f5f47d8
Le Dung <vandung2002ts@gmail.com>
2024-05-30 23:42:38 +0700

    update

--- appointment_service/appointment/models.py
+++ appointment_service/appointment/models.py
@@ -5,7 +5,7 @@ class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     doctor_id = models.CharField(max_length=10)
     patient_id = models.CharField(max_length=10, default=0)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     time = models.CharField(max_length=20, choices=[('morning', 'Morning'), ('noon', 'Noon'), ('afternoon', 'Afternoon'), ('night', 'Night')])
     description = models.CharField(max_length=1000)
     status = models.CharField(max_length=50, choices=[('scheduled', 'Scheduled'), ('completed', 'Completed'), ('cancelled', 'Cancelled')])
